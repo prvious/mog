a1699834db4fc47e6a359a8976f1cc9bf22c0839
Clovis Muneza <mugaro234@gmail.com>
2025-06-26 01:48:43 +0000

    feat: add overflow to scrollable component and default sizes

--- resources/components/scrollable.blade.php
+++ resources/components/scrollable.blade.php
@@ -1,6 +1,6 @@
 <div
     data-slot="scrollable"
-    {{ $attributes->twMerge('relative') }}>
+    {{ $attributes->twMerge('relative overflow-auto h-72 w-48') }}>
     <div
         class="focus-visible:ring-ring/50 size-full rounded-[inherit] outline-none transition-[color,box-shadow] focus-visible:outline-1 focus-visible:ring-[3px]"
         data-slot="scrollable-viewport">
