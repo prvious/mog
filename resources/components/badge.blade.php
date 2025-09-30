940009e8b56cb844cc8c1d9a66c294a8baaf144d
Clovis Muneza <mugaro234@gmail.com>
2025-09-30 04:37:59 +0000

    thick badges

--- resources/components/badge.blade.php
+++ resources/components/badge.blade.php
@@ -10,7 +10,7 @@
         default => 'bg-primary text-primary-foreground hover:bg-primary/80 border-transparent shadow',
     };
 
-    $defaultClasses = 'focus:ring-ring inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:ring-2 focus:ring-offset-2 focus:outline-none';
+    $defaultClasses = 'focus:ring-ring inline-flex items-center rounded-md border px-3 py-1 text-xs font-semibold transition-colors focus:ring-2 focus:ring-offset-2 focus:outline-none';
 @endphp
 
 <div {{ $attributes->twMerge($defaultClasses, $variantClasses) }}>{{ $slot }}</div>
