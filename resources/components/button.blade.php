a44fb677bc1c3d5b81f4f87937ef214e40a9bdd7
Clovis Muneza <mugaro234@gmail.com>
2025-12-18 06:52:09 +0000

    update button

--- resources/components/button.blade.php
+++ resources/components/button.blade.php
@@ -6,31 +6,34 @@
 
 @php
     $variantClasses = match ($variant) {
-        'destructive' => 'bg-destructive text-destructive-foreground hover:bg-destructive/90 shadow-sm',
-        'outline' => 'border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm',
-        'secondary' => 'bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-sm',
-        'ghost' => 'hover:bg-accent hover:text-accent-foreground',
+        'destructive' => 'bg-destructive hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 text-white',
+        'outline' => 'bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 border',
+        'secondary' => 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        'ghost' => 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         'link' => 'text-primary underline-offset-4 hover:underline',
-        default => 'bg-primary text-primary-foreground hover:bg-primary/90 shadow',
+        default => 'bg-primary text-primary-foreground hover:bg-primary/90',
     };
 
     $sizeClasses = match ($size) {
-        'sm' => 'h-8 rounded-md px-3 text-xs',
-        'lg' => 'h-10 rounded-md px-8',
-        'icon' => 'size-8 p-0.5',
-        default => 'h-9 px-4 py-2',
+        'sm' => 'h-8 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5',
+        'lg' => 'h-10 rounded-md px-6 has-[>svg]:px-4',
+        'icon' => 'size-9',
+        'icon-sm' => 'size-8',
+        'icon-lg' => 'size-10',
+        default => 'h-9 px-4 py-2 has-[>svg]:px-3',
     };
 
-    $iconClasses = match ($size) {
-        'sm' => 'size-3 p-0.5',
+    $loaderClasses = match ($size) {
+        'sm' => 'size-4 p-0.5',
         default => 'size-5 p-0.5',
     };
 
     $classes = [
-        'focus-visible:ring-ring group relative isolate inline-flex items-center justify-center gap-2',
-        'whitespace-nowrap rounded-md text-sm font-medium transition-colors duration-75',
-        'hover:cursor-pointer focus-visible:outline-none focus-visible:ring-1',
-        'disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-3 [&_svg]:shrink-0',
+        'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive shrink-0',
+        'focus-visible:ring-ring/50 group/button relative isolate inline-flex items-center justify-center gap-2',
+        'whitespace-nowrap rounded-md font-medium transition-all duration-75',
+        'focus-visible:border-ring text-sm outline-none focus-visible:ring-[3px]',
+        "disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0",
     ];
 
     $loading = $attributes->whereStartsWith('wire:click')->first() !== null || $attributes->has('wire:target');
@@ -58,9 +61,9 @@
 <{{ $tag }} {{ $attributes->twMerge($classes, $variantClasses, $sizeClasses) }}>
     @if ($loading)
         <div
-            class="group-data-loading:animate-spin absolute inset-0 flex items-center justify-center opacity-0"
+            class="group-data-loading/button:animate-spin absolute inset-0 flex items-center justify-center opacity-0"
             data-loading-indicator>
-            @svg('mog-loader-2', $iconClasses)
+            @svg('mog-loader-2', $loaderClasses)
         </div>
     @endif
 
