01c0fe567036666d16af7cbcf07894d70644f1e0
Clovis Muneza <mugaro234@gmail.com>
2025-12-21 03:23:44 +0000

    refactor: improved accordion attribute handling

--- resources/components/accordion.blade.php
+++ resources/components/accordion.blade.php
@@ -14,21 +14,24 @@
 @endphp
 
 <div
-    data-mog-groupable
+    data-ignore
     data-slot="accordion"
     x-data="{
         open: @js($open),
     }"
     x-modelable="open"
     {{ $attributes->cn('border-b group')->merge(['x-bind:data-state' => "open ? 'open' : 'closed'"]) }}>
-    <div class="flex">
-        <button
-            {!! when($collapsible !== false, 'x-on:click="open = !open"') !!}
-            {{ $trigger->attributes->cn('flex flex-1 items-center justify-between py-4 text-left text-sm font-medium transition-[rotate] hover:underline group-data-[state=closed]:[&>svg]:rotate-180 [&>svg]:transition-[rotate] [&>svg]:duration-200') }}>
-            {{ $trigger }}
-            {{ svg('mog-chevron-up', 'text-muted-foreground h-4 w-4 shrink-0') }}
-        </button>
-    </div>
+    <button
+        data-ignore-trigger
+        {{
+            $trigger->attributes
+                ->merge(['data-slot' => 'accordion-trigger', 'type' => 'button'])
+                ->when($collapsible !== false, fn ($attr) => $attr->merge(['x-on:click' => 'open = !open']))
+                ->cn('flex flex-1 items-center justify-between py-4 text-left text-sm font-medium transition-[rotate] hover:underline group-data-[state=closed]:[&>svg]:rotate-180 [&>svg]:transition-[rotate] [&>svg]:duration-200')
+        }}>
+        {{ $trigger }}
+        {{ svg('mog-chevron-up', 'text-muted-foreground h-4 w-4 shrink-0') }}
+    </button>
 
     <div
         {{ $content->attributes->cn('grid group-data-[state=closed]:grid-rows-[0fr] transition-[grid-template-rows] ease-in-out duration-200 group-data-[state=open]:grid-rows-[1fr] overflow-hidden text-sm') }}>
