ec9663165e9957b0c0e24fb34be6909b8b2eb529
Clovis Muneza <mugaro234@gmail.com>
2025-12-21 03:33:15 +0000

    feat: add spinner component

---
+++ resources/components/spinner.blade.php
@@ -0,0 +1,5 @@
+@props([
+    'icon' => 'lucide-loader-circle',
+])
+
+@svg($icon, $attributes->cn('flex size-4 animate-spin items-center justify-center')->merge(['role' => 'status', 'aria-label' => 'Loading'])->all())
